77f4b3e25bd3eea5cb9383c16cd1fc9e9df5a18c
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2025-07-09 10:15:08 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  base:'/My-Personal-Portfolio/',
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
